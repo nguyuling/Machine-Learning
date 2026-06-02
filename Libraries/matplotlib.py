39ea547e4e503a8077477ec4e6b836899db48604
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-03 02:06:13 +0800

    add figure

--- Libraries/matplotlib.py
+++ Libraries/matplotlib.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 plt.scatter(X, y, color='red')
 plt.plot(X, y, color='red')
+plt.figure(figsize=(w,h))
 
 plt.title('title')
 plt.xlabel('X')
 plt.ylabel('y')
 plt.legend()
+
 plt.show()
