495255b5b54e91ab25060ecb38e6d13f6028bf37
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-02 14:48:25 +0800

    initial commit

---
+++ Libraries/sklearn.py
@@ -0,0 +1,31 @@
+import sklearn.preprocessing
+from sklearn.preprocessing import StandardScaler
+
+#! Model Selection
+import sklearn.model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+#! Supervised Learning
+import sklearn.linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
+import sklearn.ensemble
+from sklearn.ensemble import RandomForestClassifier 
+
+import sklearn.svm
+from sklearn.svm import SVM
+
+#! Unseupervised Learning
+import sklearn.cluster
+from sklearn.cluster import KMeans
+
+#! Model Evaluation
+import sklearn.metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
