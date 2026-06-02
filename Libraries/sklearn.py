3098e041c0b05502465e5bd4d7db5570788116d4
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-03 02:29:55 +0800

    updated metrics

--- Libraries/sklearn.py
+++ Libraries/sklearn.py
@@ -43,8 +43,11 @@ from sklearn.cluster import KMeans
 # Model Evaluation
 import sklearn.metrics
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import r2_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
