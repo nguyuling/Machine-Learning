04730b3bb0a086e907c714b35f36a782748b929b
Yuling👩🏻‍💻 <nguyuling@graduate.utm.my>
2026-06-02 23:55:44 +0800

    completed

--- Supervised Learning/linear-reg.py
+++ Supervised Learning/linear-reg.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # load data
 path = 'Data/Salary_Data.csv'
@@ -11,6 +11,13 @@ print(dataset.head(3))
 # define feature and target
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+# scatter plot of train test data
+plt.scatter(X, y, color='green')
+plt.title('Salary vs Experience')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary (INR)')
+# plt.show()
 
 # train test split
 from sklearn.model_selection import train_test_split
@@ -32,9 +39,13 @@ print('MSE: ', mean_squared_error(y_test, y_pred))
 print('MAE: ', mean_absolute_error(y_test, y_pred))
 print('R2 score: ', r2_score(y_test, y_pred))
 
-# scatter plot of train test data
-plt.scatter(X, y, color='green')
-plt.title('Salary vs Experience')
+# data visualization (actual vs predicted)
+y_pred = regressor.predict(X)
+plt.scatter(X, y, color='green', label='actual data points')
+plt.scatter(X, y_pred, color='blue', label='predicted data points')
+plt.plot(X, y_pred, color='red')
+plt.title('Salary vs Experience (All Dataset)')
 plt.xlabel('Years of Experience')
-plt.ylabel('Salary (INR)')
-# plt.show()
+plt.ylabel('Salary (Rupees)')
+plt.legend()
+plt.show()
